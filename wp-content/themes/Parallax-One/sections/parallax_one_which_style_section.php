9df17cce581bac01b25ab958985f352fc881a39d
Frantisek Hajnovic <ferohajnovic@gmail.com>
2015-10-20 11:59:47 +0100

    replaced tabs with spaces. Further indentation

--- wp-content/themes/Parallax-One/sections/parallax_one_which_style_section.php
+++ wp-content/themes/Parallax-One/sections/parallax_one_which_style_section.php
@@ -2,9 +2,9 @@
  SECTION: WHICH STYLE
 ========================== -->
 <?php
-	$which_style_title = get_theme_mod('which_style_title','DANCE4WATER STYLES');
-	$which_style_subtitle = get_theme_mod('which_style_subtitle','Some introductory text');
-	$which_style_dances = get_theme_mod(
+  $which_style_title = get_theme_mod('which_style_title','DANCE4WATER STYLES');
+  $which_style_subtitle = get_theme_mod('which_style_subtitle','Some introductory text');
+  $which_style_dances = get_theme_mod(
     'which_style_content', 
     json_encode(
       array(
@@ -25,71 +25,71 @@
         )
       )
     )
-	);
+  );
   
-	if(!empty($which_style_title) || 
+  if(!empty($which_style_title) || 
      !empty($which_style_subtitle) ||
      !empty($which_style_dances)) {
 ?>
-		<section class="which_style" id="which_style">
-			<div class="section-overlay-layer">
-				<div class="container">
+    <section class="which_style" id="which_style">
+      <div class="section-overlay-layer">
+        <div class="container">
 
-					<!-- HEADER -->
-					<div class="section-header">
+          <!-- HEADER -->
+          <div class="section-header">
 <?php
-							if(!empty($which_style_title)) {
-								echo '<h2 class="dark-text">'.esc_attr($which_style_title).'</h2>';
-							} elseif (isset($wp_customize)) {
-								echo '<h2 class="dark-text paralax_one_only_customizer"></h2>';
-                echo '<div class="colored-line paralax_one_only_customizer"></div>';
-							}
-?>
+            if(!empty($which_style_title)) {
+              echo '<h2 class="dark-text">'.esc_attr($which_style_title).'</h2>';
+            } elseif (isset($wp_customize)) {
+              echo '<h2 class="dark-text paralax_one_only_customizer"></h2>';
+              echo '<div class="colored-line paralax_one_only_customizer"></div>';
+            }
+  ?>
 
-<?php
-							if(!empty($which_style_subtitle)) {
-								echo '<div class="sub-heading">'.esc_attr($which_style_subtitle).'</div>';
-							} elseif (isset($wp_customize)) {
-								echo '<div class="sub-heading paralax_one_only_customizer"></div>';
-							}
+  <?php
+            if(!empty($which_style_subtitle)) {
+              echo '<div class="sub-heading">'.esc_attr($which_style_subtitle).'</div>';
+            } elseif (isset($wp_customize)) {
+              echo '<div class="sub-heading paralax_one_only_customizer"></div>';
+            }
 ?>
-					</div>
+          </div>
 
           <!-- DANCE STYLES -->
 <?php
-						if(!empty($which_style_dances)) {
-							$which_style_decoded = json_decode($which_style_dances);
-							echo '<div id="which_style_wrap" class="which_style-wrap">';
-                $counter = 0;
-								foreach($which_style_decoded as $service_box) {
-									if(!empty($service_box->video_url)
-                      || !empty($service_box->title) 
-                      || !empty($service_box->text)) {
-										echo '<div class="service-box"><div class="single-service border-bottom-hover">';
-                    if(!empty($service_box->title)) {
-                      echo '<h3 class="colored-text">'.esc_attr($service_box->title).'</h3>';
-                    }
+          if(!empty($which_style_dances)) {
+            $which_style_decoded = json_decode($which_style_dances);
+            echo '<div id="which_style_wrap" class="which_style-wrap">';
+              $counter = 0;
+              foreach($which_style_decoded as $service_box) {
+                if(!empty($service_box->video_url)
+                    || !empty($service_box->title) 
+                    || !empty($service_box->text)) {
+                  echo '<div class="service-box"><div class="single-service border-bottom-hover">';
+                  if(!empty($service_box->title)) {
+                    echo '<h3 class="colored-text">'.esc_attr($service_box->title).'</h3>';
+                  }
 
-                    if(!empty($service_box->text)) {
-                      echo '<p>'. esc_attr($service_box->text).'</p>';
-                    }
+                  if(!empty($service_box->text)) {
+                    echo '<p>'. esc_attr($service_box->text).'</p>';
+                  }
 
-                    $counter++;
-                    if( !empty($service_box->video_url)) {
-                      echo '<iframe width="100%" id="frame'.$counter.'" src="'.esc_url($service_box->video_url).'" frameborder="0" allowfullscreen></iframe>';
-                      echo '<script type="text/javascript">';
-                      echo 'document.getElementById("frame'.$counter.'").src=document.getElementById("frame'.$counter.'").src;';
-                      echo '</script>';
-                    }
-										echo '</div></div>';
-									}
-								}
-							echo '</div>';
-						}
+                  $counter++;
+                  if( !empty($service_box->video_url)) {
+                    echo '<iframe width="100%" id="frame'.$counter.'" src="'.esc_url($service_box->video_url).'" frameborder="0" allowfullscreen></iframe>';
+                    echo '<script type="text/javascript">';
+                    echo 'document.getElementById("frame'.$counter.'").src=document.getElementById("frame'.$counter.'").src;';
+                    echo '</script>';
+                  }
+                  echo '</div></div>';
+                }
+              }
+            echo '</div>';
+          }
 ?>
-				</div>	
-			</div>
-		</section>
+        </div>  
+      </div>
+    </section>
 <?php
-	}
+  }
 ?>
--- wp-content/themes/Parallax-One/sections/parallax_one_why_us_section.php
+++ wp-content/themes/Parallax-One/sections/parallax_one_why_us_section.php
@@ -2,8 +2,8 @@
  SECTION: WHY US
 ========================== -->
 <?php
-	$why_us_title = get_theme_mod('why_us_title','WHY DANCE WITH US?');
-	$why_us_content = get_theme_mod(
+  $why_us_title = get_theme_mod('why_us_title','WHY DANCE WITH US?');
+  $why_us_content = get_theme_mod(
     'why_us_content', 
     json_encode(
       array(
@@ -26,57 +26,57 @@
     )
   );
   
-	if(!empty($why_us_title) || 
+  if(!empty($why_us_title) || 
      !empty($why_us_content)) {
 ?>
-		<section class="why_us" id="why_us">
-			<div class="section-overlay-layer">
-				<div class="container">
+    <section class="why_us" id="why_us">
+      <div class="section-overlay-layer">
+        <div class="container">
 
-					<!-- HEADER -->
-					<div class="section-header">
+          <!-- HEADER -->
+          <div class="section-header">
 <?php
-							if(!empty($why_us_title)) {
-								echo '<h2 class="dark-text">'.esc_attr($why_us_title).'</h2>';
-							} elseif (isset($wp_customize)) {
-								echo '<h2 class="dark-text paralax_one_only_customizer"></h2>';
-                echo '<div class="colored-line paralax_one_only_customizer"></div>';
-							}
+            if(!empty($why_us_title)) {
+              echo '<h2 class="dark-text">'.esc_attr($why_us_title).'</h2>';
+            } elseif (isset($wp_customize)) {
+              echo '<h2 class="dark-text paralax_one_only_customizer"></h2>';
+              echo '<div class="colored-line paralax_one_only_customizer"></div>';
+            }
 ?>
-					</div>
+          </div>
 
           <!-- WHY US CONTENT-->
 <?php
-						if(!empty($why_us_content)) {
-							$why_us_decoded = json_decode($why_us_content);
-              $width_perc = 90 / count($why_us_decoded);
-							echo '<div id="why-us-wrap" class="why-us-wrap">';
-                $counter = 0;
-								foreach($why_us_decoded as $service_box) {
-									if(!empty($service_box->image_url)
-                      || !empty($service_box->title) 
-                      || !empty($service_box->text)) {
-										echo '<div class="why-us-box" style="width:'.$width_perc.'%">';
-                    if(!empty($service_box->image_url)) {
-                      echo '<img class="why-us-img" src="'.$service_box->image_url.'" alt="'.$service_box->title.'">';
-                    }
+          if(!empty($why_us_content)) {
+            $why_us_decoded = json_decode($why_us_content);
+            $width_perc = 90 / count($why_us_decoded);
+            echo '<div id="why-us-wrap" class="why-us-wrap">';
+              $counter = 0;
+              foreach($why_us_decoded as $service_box) {
+                if(!empty($service_box->image_url)
+                    || !empty($service_box->title) 
+                    || !empty($service_box->text)) {
+                  echo '<div class="why-us-box" style="width:'.$width_perc.'%">';
+                  if(!empty($service_box->image_url)) {
+                    echo '<img class="why-us-img" src="'.$service_box->image_url.'" alt="'.$service_box->title.'">';
+                  }
 
-                    if(!empty($service_box->title)) {
-                      echo '<h3 class="colored-text">'.esc_attr($service_box->title).'</h3>';
-                    }
+                  if(!empty($service_box->title)) {
+                    echo '<h3 class="colored-text">'.esc_attr($service_box->title).'</h3>';
+                  }
 
-                    if(!empty($service_box->text)) {
-                      echo '<p>'. esc_attr($service_box->text).'</p>';
-                    }
-										echo '</div>';
-									}
-								}
-							echo '</div>';
-						}
+                  if(!empty($service_box->text)) {
+                    echo '<p>'. esc_attr($service_box->text).'</p>';
+                  }
+                  echo '</div>';
+                }
+              }
+            echo '</div>';
+          }
 ?>
-				</div>	
-			</div>
-		</section>
+        </div>  
+      </div>
+    </section>
 <?php
-	}
+  }
 ?>
