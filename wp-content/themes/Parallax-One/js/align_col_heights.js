853d4ef5695d690d677f45b8d0d8aaa6d3f4cb72
Frantisek Hajnovic <ferohajnovic@gmail.com>
2015-11-16 09:51:30 +0000

    javascript/jquery for aligning column heights simplified

--- wp-content/themes/Parallax-One/js/align_col_heights.js
+++ wp-content/themes/Parallax-One/js/align_col_heights.js
@@ -6,23 +6,25 @@
  * See e.g. parallax_one_which_style_section.php for example of usage
  * */
 function align_col_heights(row_class, col_class) {
-  var rows_len = $('.' + row_class).length;
+  var rows = $('.' + row_class);
+  var rows_len = rows.length;
   for (var i = 0; i < rows_len; i++) {               
     //reset the height to initial - heights will now scale as needed 
-    $('.' + row_class).eq(i).find('.' + col_class).css({
+    var cols = rows.eq(i).find('.' + col_class);
+    cols.css({
       'height': 'initial'
     });
 
     //now get the maximum height
-    var boxes_len = $('.' + row_class).eq(i).find('.' + col_class).length;
+    var boxes_len = cols.length;
     var max_height = 0;
     for (var j = 0; j < boxes_len; j++) {
-      var h = $('.' + row_class).eq(i).find('.' + col_class).eq(j).outerHeight();
+      var h = cols.eq(j).outerHeight();
       max_height = Math.max(max_height, h);
     }
 
     //set all heights to the found max
-    $('.' + row_class).eq(i).find('.' + col_class).css({
+    cols.css({
       'height': max_height + 'px'
     });
   }
