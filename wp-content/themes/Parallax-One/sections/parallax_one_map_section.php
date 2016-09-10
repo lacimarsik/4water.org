6e79aeb9f84bc2effe0481d32ea6601edf69fdbf
lacimarsik <laci.marsik@gmail.com>
2016-09-10 21:13:45 +0200

    Map height: 400px

--- wp-content/themes/Parallax-One/sections/parallax_one_map_section.php
+++ wp-content/themes/Parallax-One/sections/parallax_one_map_section.php
@@ -5,6 +5,12 @@ INTERGEO MAPS
   $parallax_one_contact_map_shortcode = get_theme_mod('parallax_one_contact_map_shortcode');
   if( !empty($parallax_one_contact_map_shortcode) ){
 ?>
+    <!-- OVERRIDE MAP STYLING -->
+    <style>
+      #cd-google-map .intergeo_map_canvas {
+        height: 400px !important;
+      }
+    </style>
     <div id="container-fluid">
       <div class="parallax_one_map_overlay"></div>
       <div id="cd-google-map">
