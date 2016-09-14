8a64f6d5b8ed38d4f509f3ce9fa0d8e614bb988a
Ladislav Marsik <laci.marsik@gmail.com>
2016-09-14 08:54:50 +0000

    Yoga4Water Calendar

--- wp-content/themes/Parallax-One/inc/calendar/google_calendar_api.php
+++ wp-content/themes/Parallax-One/inc/calendar/google_calendar_api.php
@@ -22,7 +22,7 @@
       "/lyon/dance/fr/" => "6vuhl5mspdfs9tht3tlgadenn8@group.calendar.google.com",
       "/lyon/climbing/" => "6vuhl5mspdfs9tht3tlgadenn8@group.calendar.google.com",
       "/lyon/climbing/fr/" => "6vuhl5mspdfs9tht3tlgadenn8@group.calendar.google.com",
-      "/manchester/yoga/" => "6vuhl5mspdfs9tht3tlgadenn8@group.calendar.google.com",
+      "/manchester/yoga/" => "i7bmccp5jqbcuvshjqr45pl4hc@group.calendar.google.com",
       "/prague/dance/" => "p3insueeqtr06h7l658qj1aqi0@group.calendar.google.com",
       "/prague/dance/cz/" => "19risi6pjr73kgg4dja1j18c2o@group.calendar.google.com",
       "/sample/" => "6vuhl5mspdfs9tht3tlgadenn8@group.calendar.google.com"
