42b28557ae118df24898f1aa92d02347862c9a7d
lacimarsik <laci.marsik@gmail.com>
2016-09-23 17:00:00 +0200

    Berlin4Water public calendar

--- wp-content/themes/Parallax-One/inc/calendar/google_calendar_api.php
+++ wp-content/themes/Parallax-One/inc/calendar/google_calendar_api.php
@@ -25,7 +25,8 @@
       "/manchester/yoga/" => "i7bmccp5jqbcuvshjqr45pl4hc@group.calendar.google.com",
       "/prague/dance/" => "p3insueeqtr06h7l658qj1aqi0@group.calendar.google.com",
       "/prague/dance/cz/" => "19risi6pjr73kgg4dja1j18c2o@group.calendar.google.com",
-      "/berlin/" => "eb2loeck1dsfjlmc2q1000rb2o@group.calendar.google.com",
+      "/berlin/" => "bakucjkss4lslq3djtfu4fi5pg@group.calendar.google.com",
+      "/berlin-volunteers/" => "eb2loeck1dsfjlmc2q1000rb2o@group.calendar.google.com",
       "/sample/" => "ba6a63k1ctd20v0amckvo7pp60@group.calendar.google.com"
     );
 
