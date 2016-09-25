f3f27664f9536bdddf2bfcd6b9c8f70bc2992f43
lacimarsik <laci.marsik@gmail.com>
2016-09-25 21:44:55 +0200

    Fix for hiding This week / Next week

--- wp-content/themes/Parallax-One/inc/calendar/frontend/directives/calendarWeekSwitchDirective.js
+++ wp-content/themes/Parallax-One/inc/calendar/frontend/directives/calendarWeekSwitchDirective.js
@@ -1,17 +1,20 @@
 (function () {
     var app = angular.module('4water.directives');
 
-    app.directive('calendarWeekSwitch', function () {
-        return {
-            restrict: 'E',
-            link: function(scope) {
-                scope.getButtonTranslation = function(idOfTranslationDiv) {
-                    // get translation from Customizer
-                    var translationDiv = document.getElementById(idOfTranslationDiv);
-                    return translationDiv.textContent;
-                }
-            },
-            templateUrl: 'wp-content/themes/Parallax-One/inc/calendar/frontend/templates/calendarWeekSwitchTemplate.html'
-        };
-    });
+    if (document.getElementById('calendar-this-week') && document.getElementById('calendar-next-week')) {
+        app.directive('calendarWeekSwitch', function () {
+                return {
+                    restrict: 'E',
+                    link: function (scope) {
+                        scope.getButtonTranslation = function (idOfTranslationDiv) {
+                            // get translation from Customizer
+                            var translationDiv = document.getElementById(idOfTranslationDiv);
+                            return translationDiv.textContent;
+                        }
+                    },
+                    templateUrl: 'wp-content/themes/Parallax-One/inc/calendar/frontend/templates/calendarWeekSwitchTemplate.html'
+                };
+            }
+        );
+    }
 })();
