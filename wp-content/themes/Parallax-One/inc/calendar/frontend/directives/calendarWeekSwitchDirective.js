f94d1f9e36db498b52f4934179486ce016e1b8b4
lacimarsik <laci.marsik@gmail.com>
2016-09-26 00:29:58 +0200

    Fix lost Calendar buttons This week / Next week

--- wp-content/themes/Parallax-One/inc/calendar/frontend/directives/calendarWeekSwitchDirective.js
+++ wp-content/themes/Parallax-One/inc/calendar/frontend/directives/calendarWeekSwitchDirective.js
@@ -1,20 +1,21 @@
 (function () {
     var app = angular.module('4water.directives');
 
-    if (document.getElementById('calendar-this-week') && document.getElementById('calendar-next-week')) {
-        app.directive('calendarWeekSwitch', function () {
-                return {
-                    restrict: 'E',
-                    link: function (scope) {
-                        scope.getButtonTranslation = function (idOfTranslationDiv) {
-                            // get translation from Customizer
-                            var translationDiv = document.getElementById(idOfTranslationDiv);
-                            return translationDiv.textContent;
-                        }
-                    },
-                    templateUrl: 'wp-content/themes/Parallax-One/inc/calendar/frontend/templates/calendarWeekSwitchTemplate.html'
-                };
+    app.directive('calendarWeekSwitch', function () {
+            var templateUrl = null;
+            if ((document.getElementById('calendar-this-week') != undefined) && (document.getElementById('calendar-next-week') != undefined)) {
+                templateUrl = 'wp-content/themes/Parallax-One/inc/calendar/frontend/templates/calendarWeekSwitchTemplate.html';
             }
-        );
-    }
+            return {
+                restrict: 'E',
+                link: function (scope) {
+                    scope.getButtonTranslation = function (idOfTranslationDiv) {
+                        // get translation from Customizer
+                        var translationDiv = document.getElementById(idOfTranslationDiv);
+                        return translationDiv.textContent;
+                    }
+                },
+                templateUrl:  templateUrl
+        }
+    );
 })();
