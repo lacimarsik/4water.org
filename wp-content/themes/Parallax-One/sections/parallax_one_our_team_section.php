d6659c02f982236e9447060c7374b94e6451e171
lacimarsik <laci.marsik@gmail.com>
2016-09-25 16:18:11 +0200

    Team section below Prices

--- wp-content/themes/Parallax-One/sections/parallax_one_our_team_section.php
+++ wp-content/themes/Parallax-One/sections/parallax_one_our_team_section.php
@@ -4,15 +4,7 @@
 <?php
 	$parallax_one_our_team_title = get_theme_mod('parallax_one_our_team_title','Our Team');
 	$parallax_one_our_team_subtitle = get_theme_mod('parallax_one_our_team_subtitle','Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
-	$parallax_one_team_content = get_theme_mod('parallax_one_team_content',
-		json_encode(
-			array(
-					array('image_url' => parallax_get_file('/images/team/1.jpg'),'title' => esc_html__('Albert Jacobs','parallax-one'),'subtitle' => esc_html__('Founder & CEO','parallax-one')),
-					array('image_url' => parallax_get_file('/images/team/2.jpg'),'title' => esc_html__('Tonya Garcia','parallax-one'),'subtitle' => esc_html__('Account Manager','parallax-one')),
-					array('image_url' => parallax_get_file('/images/team/3.jpg'),'title' => esc_html__('Linda Guthrie','parallax-one'),'subtitle' => esc_html__('Business Development','parallax-one'))
-			)
-		)
-	);
+	$parallax_one_team_content = get_theme_mod('parallax_one_team_content', DefTeam::$content);
 
 	if(!empty($parallax_one_our_team_title) || !empty($parallax_one_our_team_subtitle) || !empty($parallax_one_team_content)){
 ?>
@@ -26,9 +18,9 @@
 							<div class="section-header">
 							<?php
 								if( !empty($parallax_one_our_team_title) ){
-									echo '<h2 class="dark-text">'.esc_attr($parallax_one_our_team_title).'</h2><div class="colored-line"></div>';
+									echo '<h2 class="dark-text">'.esc_attr($parallax_one_our_team_title).'</h2>';
 								} elseif ( isset( $wp_customize )   ) {
-									echo '<h2 class="dark-text paralax_one_only_customizer"></h2><div class="colored-line paralax_one_only_customizer"></div>';
+									echo '<h2 class="dark-text paralax_one_only_customizer"></h2>';
 								}
 
 							?>
@@ -68,13 +60,13 @@
 												<div class="member-details-inner">
 													<?php 
 													if( !empty($parallax_one_team_member->title) ){?>
-														<h5 class="colored-text"><?php echo esc_attr($parallax_one_team_member->title);?></h5>
+														<h5 class="colored-text"><?php echo nl2br(esc_attr($parallax_one_team_member->title));?></h5>
 													<?php 
 													}
 													if( !empty($parallax_one_team_member->subtitle) ){ ?>
 														<div class="small-text">
 															<?php 
-															   echo esc_attr($parallax_one_team_member->subtitle);
+															   echo nl2br(esc_attr($parallax_one_team_member->subtitle));
 															?>	
 														</div>
 
