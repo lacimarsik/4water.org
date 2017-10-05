6b536fbd5bc33f2572bcf3fa2799237cf24b1c18
lacimarsik <laci.marsik@gmail.com>
2017-10-05 08:45:07 +0200

    Subscription - fixes (url, styles)

--- wp-content/themes/Parallax-One/sections/parallax_one_subscription_section.php
+++ wp-content/themes/Parallax-One/sections/parallax_one_subscription_section.php
@@ -28,20 +28,20 @@ if(!empty($subscription_title) ||
 				</div>
 
 				<div class="subscription">
-					<link href="<?php echo $subscription_url; ?>" rel="stylesheet" type="text/css">
+					<!-- Do not use Mailchimp styles <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css"> -->
 					<style type="text/css">
 						#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
 						/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
 						   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 					</style>
 					<div id="mc_embed_signup">
-						<form action="//4water.us15.list-manage.com/subscribe/post?u=5410f150f2af255024e9e7612&amp;id=e3f5e363b0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						<form action="<?php echo $subscription_url; ?>" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 							<div id="mc_embed_signup_scroll" style="background: #F7F8FA;">
 								<label for="mce-EMAIL"></label>
 								<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required style="text-align: center; margin-bottom: 10px; width: 300px;">
 								<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 								<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5410f150f2af255024e9e7612_e3f5e363b0" tabindex="-1" value=""></div>
-								<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+								<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" style="margin-bottom: 20px;"></div>
 							</div>
 						</form>
 					</div>
