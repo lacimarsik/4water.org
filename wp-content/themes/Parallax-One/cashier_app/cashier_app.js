7ffe2fe5ae203d00086cfc387a1368a7ef0635c8
lacimarsik <laci.marsik@gmail.com>
2017-10-09 11:22:15 +0200

    Cashier app: Notify about erasing data

--- wp-content/themes/Parallax-One/cashier_app/cashier_app.js
+++ wp-content/themes/Parallax-One/cashier_app/cashier_app.js
@@ -74,7 +74,7 @@ function getValuesFromServer() {
         data = JSON.parse(data);
         var volunteer_name = data['volunteer_name'];
         if (volunteer_name != $('#name').val()) {
-          alert("This class was already counted by: " + volunteer_name + ". Making changes overwrite the previous count.")
+          alert("This class was already counted by: " + volunteer_name + ". Making changes will overwrite the previous count. Note that even hitting + or - already updates the data in the database.")
         } else {
           alert("Previous counts were found and were loaded.")
         }
