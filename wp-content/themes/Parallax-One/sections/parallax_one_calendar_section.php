147078da50ee26a4626e88d3e86705a1ce8de924
lacimarsik <laci.marsik@gmail.com>
2018-09-11 13:23:30 +0200

    Minor improvements

--- wp-content/themes/Parallax-One/sections/parallax_one_calendar_section.php
+++ wp-content/themes/Parallax-One/sections/parallax_one_calendar_section.php
@@ -7,7 +7,7 @@ $calendar_title = get_theme_mod('calendar_title', DefCalendar::$title);
 $calendar_mode = get_theme_mod('calendar_mode', DefCalendar::$mode);
 $calendar_this_week = get_theme_mod('calendar_this_week', DefCalendar::$this_week);
 $calendar_next_week = get_theme_mod('calendar_next_week', DefCalendar::$next_week);
-$calendar_fallback = get_theme_mod('calendar_fallback', DefCalendar::$fallback);
+$calendar_fallback = get_theme_mod('parallax_one_calendar_fallback', DefCalendar::$fallback);
 ?>
 
 <script src="<?= get_bloginfo("template_url"); ?>/inc/calendar/frontend/app.js"></script>
@@ -62,11 +62,11 @@ $calendar_fallback = get_theme_mod('calendar_fallback', DefCalendar::$fallback);
           <div class="for-water-calendar-ie" style="display: none;">
 <?php
                 $static_image = get_theme_mod('image_section_static_image', parallax_get_file(DefImage::$static_image));
-                if ($static_image && $calendar_fallback) {
+                if (isset($static_image) && (!empty($static_image)) && isset($calendar_fallback) && ($calendar_fallback == true)) {
                   echo '<div class="image-section-static-image"><img src="' . $static_image . '" /></div>';
                 } else {
 ?>
-                    Unfortunately, Internet Explorer / Edge cannot render the calendar properly. Please use Google Chrome or Mozilla Firefox to display the calendar.
+                    <div style="padding-bottom: 20px;">Unfortunately, Internet Explorer / Edge cannot render the calendar properly. Please use Google Chrome or Mozilla Firefox to display the calendar.</div>
 <?php
                 }
 ?>
